3355fcd956181167e2f35a858d9c7be8902ec3f0
mrdevil786 <suhailsaeedme@gmail.com>
2024-01-14 16:15:13 +0530

    Added Auth Controller

---
+++ app/Http/Controllers/Web/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function loginpage()
+    {
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($request->only('email', 'password'))) {
+            $request->user()->createToken('auth-token')->plainTextToken;
+
+            return redirect()->route('dashboard')->with('success', 'Successfully logged in.');
+        }
+
+        return redirect()->route('login')->with('danger', 'Invalid credentials.');
+    }
+
+    public function logout()
+    {
+        Auth::guard('web')->logout();
+
+        request()->session()->invalidate();
+        request()->session()->regenerateToken();
+
+        return redirect()->route('login')->with('success', 'Successfully logged out.');
+    }
+}
