21eda733bbef26e4cba194a227c34db6175da101
mrdevil786 <suhailsaeedme@gmail.com>
2024-08-03 15:06:46 +0530

    Route Optimization

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -13,25 +13,26 @@
         content="admin,admin dashboard,admin panel,admin template,bootstrap,clean,dashboard,flat,jquery,modern,responsive,premium admin templates,responsive admin,ui,ui kit.">
 
     <!-- FAVICON -->
-    <link rel="shortcut icon" type="image/x-icon" href="../assets/images/brand/favicon.ico" />
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/images/brand/favicon.ico') }}" />
 
     <!-- TITLE -->
     <title>{{ env('APP_NAME') }} – Login</title>
 
     <!-- BOOTSTRAP CSS -->
-    <link id="style" href="../assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <link id="style" href="{{ asset('assets/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" />
 
     <!-- STYLE CSS -->
-    <link href="../assets/css/style.css" rel="stylesheet" />
-    <link href="../assets/css/dark-style.css" rel="stylesheet" />
-    <link href="../assets/css/transparent-style.css" rel="stylesheet">
-    <link href="../assets/css/skin-modes.css" rel="stylesheet" />
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/dark-style.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/transparent-style.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/skin-modes.css') }}" rel="stylesheet" />
 
     <!--- FONT-ICONS CSS -->
-    <link href="../assets/css/icons.css" rel="stylesheet" />
+    <link href="{{ asset('assets/css/icons.css') }}" rel="stylesheet" />
 
     <!-- COLOR SKIN CSS -->
-    <link id="theme" rel="stylesheet" type="text/css" media="all" href="../assets/colors/color1.css" />
+    <link id="theme" rel="stylesheet" type="text/css" media="all"
+        href="{{ asset('assets/colors/color1.css') }}" />
 
 </head>
 
@@ -53,14 +54,15 @@
                 <!-- CONTAINER OPEN -->
                 <div class="col col-login mx-auto mt-7">
                     <div class="text-center">
-                        <a href="{{ url('/') }}"><img src="../assets/images/brand/logo-white.png"
+                        <a href="{{ url('/') }}"><img src="{{ asset('assets/images/brand/logo-white.png') }}"
                                 class="header-brand-img" alt=""></a>
                     </div>
                 </div>
 
                 <div class="container-login100">
                     <div class="wrap-login100 p-6">
-                        <form class="login100-form validate-form" method="POST" action="{{ route('admin.submit.login') }}">
+                        <form class="login100-form validate-form" method="POST"
+                            action="{{ route('admin.submit.login') }}">
                             @csrf
                             <span class="login100-form-title pb-5">
                                 {{ env('APP_NAME') }} – Login
@@ -139,26 +141,26 @@
     <!-- BACKGROUND-IMAGE CLOSED -->
 
     <!-- JQUERY JS -->
-    <script src="../assets/js/jquery.min.js"></script>
+    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
 
     <!-- BOOTSTRAP JS -->
-    <script src="../assets/plugins/bootstrap/js/popper.min.js"></script>
-    <script src="../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <script src="{{ asset('assets/plugins/bootstrap/js/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
 
     <!-- SHOW PASSWORD JS -->
-    <script src="../assets/js/show-password.min.js"></script>
+    <script src="{{ asset('assets/js/show-password.min.js') }}"></script>
 
     <!-- GENERATE OTP JS -->
-    <script src="../assets/js/generate-otp.js"></script>
+    <script src="{{ asset('assets/js/generate-otp.js') }}"></script>
 
-    <!-- Perfect SCROLLBAR JS-->
-    <script src="../assets/plugins/p-scroll/perfect-scrollbar.js"></script>
+    <!-- Perfect SCROLLBAR JS -->
+    <script src="{{ asset('assets/plugins/p-scroll/perfect-scrollbar.js') }}"></script>
 
-    <!-- Color Theme js -->
-    <script src="../assets/js/themeColors.js"></script>
+    <!-- Color Theme JS -->
+    <script src="{{ asset('assets/js/themeColors.js') }}"></script>
 
     <!-- CUSTOM JS -->
-    <script src="../assets/js/custom.js"></script>
+    <script src="{{ asset('assets/js/custom.js') }}"></script>
 
     <script>
         document.addEventListener('DOMContentLoaded', function() {
