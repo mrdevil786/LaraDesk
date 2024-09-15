3e3d6747dc882a6fbd53fc47f0c171208295b8ee
mrdevil786 <suhailsaeedme@gmail.com>
2024-09-15 14:15:11 +0530

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export default defineConfig({
     plugins: [
